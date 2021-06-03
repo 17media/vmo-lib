f5dbc3227902fbced5030384dba7a286fa334935
sky790312 <sky790312yks@gmail.com>
2021-06-03 18:26:18 +0800

    fix: prevent TransitionLeaderboardWrapper rerender by outside

--- lib/components/TransitionLeaderboardWrapper.tsx
+++ lib/components/TransitionLeaderboardWrapper.tsx
@@ -19,7 +19,7 @@ const transitionStyle = {
   transition: "all 0.5s ease 0.3s",
 };
 
-export const TransitionLeaderboardWrapper: React.FC<Props> = ({
+export const TransitionLeaderboardWrapper: React.FC<Props> = React.memo(({
   user,
   itemStyle,
   rowCount,
@@ -46,6 +46,6 @@ export const TransitionLeaderboardWrapper: React.FC<Props> = ({
   }
 
   return <Wrapper>{renderChild()}</Wrapper>;
-};
+});
 
 export default TransitionLeaderboardWrapper;
