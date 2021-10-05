d23b197c95ab707a822edaf04c589b8734bf009f
David Hung <davidhung@17.media>
2021-10-05 15:29:09 +0800

    feat: add no filtered data test

--- lib/hooks/__test__/filter.test.ts
+++ lib/hooks/__test__/filter.test.ts
@@ -5,72 +5,71 @@ import useFilter from '../useFilter';
 import { User } from '../../types';
 
 jest.mock('17media-browser-spy');
+const leaderboardData: User[] = [
+  {
+    userInfo: {
+      userID: '4bfcd001-5ff1-4ec7-88fe-e395b05386b0',
+      displayName: '我是柔柔',
+      picture: '24661c0e-a4da-4d34-8d5b-f85ba5bf98bc.jpg',
+      name: '',
+      level: 0,
+      openID: '123',
+      region: 'TW',
+      gloryroadMode: 0,
+    },
+    score: 1000000,
+    rank: 1,
+    meta: { eventoryKey: '4bfcd001-5ff1-4ec7-88fe-e395b05386b0' },
+  },
+  {
+    userInfo: {
+      userID: 'ewe123',
+      displayName: '我是硬硬且剛剛',
+      picture: '789',
+      name: '',
+      level: 0,
+      openID: '789',
+      region: 'TW',
+      gloryroadMode: 0,
+    },
+    score: 1000000,
+    rank: 2,
+    meta: { eventoryKey: '1234' },
+  },
+  {
+    userInfo: {
+      userID: '123gwe4',
+      displayName: '我是剛剛',
+      picture: '78910',
+      name: '',
+      level: 0,
+      openID: '7894',
+      region: 'TW',
+      gloryroadMode: 0,
+    },
+    score: 1000000,
+    rank: 3,
+    meta: { eventoryKey: '1235' },
+  },
+  {
+    userInfo: {
+      userID: '123dd4',
+      displayName: '我是軟軟',
+      picture: '7891ee0',
+      name: '',
+      level: 0,
+      openID: '7894',
+      region: 'TW',
+      gloryroadMode: 0,
+    },
+    score: 1000000,
+    rank: 4,
+    meta: { eventoryKey: '123we5' },
+  },
+];
 
 describe('test filter hook', () => {
   test('should get filtered data length equals to two', async () => {
-    const leaderboardData: User[] = [
-      {
-        userInfo: {
-          userID: '4bfcd001-5ff1-4ec7-88fe-e395b05386b0',
-          displayName: '我是柔柔',
-          picture: '24661c0e-a4da-4d34-8d5b-f85ba5bf98bc.jpg',
-          name: '',
-          level: 0,
-          openID: '123',
-          region: 'TW',
-          gloryroadMode: 0,
-        },
-        score: 1000000,
-        rank: 1,
-        meta: { eventoryKey: '4bfcd001-5ff1-4ec7-88fe-e395b05386b0' },
-      },
-      {
-        userInfo: {
-          userID: 'ewe123',
-          displayName: '我是硬硬且剛剛',
-          picture: '789',
-          name: '',
-          level: 0,
-          openID: '789',
-          region: 'TW',
-          gloryroadMode: 0,
-        },
-        score: 1000000,
-        rank: 2,
-        meta: { eventoryKey: '1234' },
-      },
-      {
-        userInfo: {
-          userID: '123gwe4',
-          displayName: '我是剛剛',
-          picture: '78910',
-          name: '',
-          level: 0,
-          openID: '7894',
-          region: 'TW',
-          gloryroadMode: 0,
-        },
-        score: 1000000,
-        rank: 3,
-        meta: { eventoryKey: '1235' },
-      },
-      {
-        userInfo: {
-          userID: '123dd4',
-          displayName: '我是軟軟',
-          picture: '7891ee0',
-          name: '',
-          level: 0,
-          openID: '7894',
-          region: 'TW',
-          gloryroadMode: 0,
-        },
-        score: 1000000,
-        rank: 4,
-        meta: { eventoryKey: '123we5' },
-      },
-    ];
-
     const { result, waitForNextUpdate } = renderHook(() =>
       useFilter(leaderboardData),
     );
@@ -80,5 +79,17 @@ describe('test filter hook', () => {
 
     expect(createSearchAction).toHaveBeenCalled();
     expect(result.current.data.length).toEqual(2);
+  });
+
+  test('should get no filtered data', async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFilter(leaderboardData),
+    );
+
+    result.current.handleOnChange('0');
+    await waitForNextUpdate();
+
+    expect(createSearchAction).toHaveBeenCalled();
+    expect(result.current.data.length).toEqual(0);
   });
 });
