c8c18948b0badde9cf2b58a2dd980a504c087083
davidhung17LIVE <88997624+davidhung17LIVE@users.noreply.github.com>
2021-10-05 16:57:13 +0800

    feat: add type doc (#22)

--- lib/hooks/useFilter.ts
+++ lib/hooks/useFilter.ts
@@ -5,6 +5,12 @@ import { User as LeaderboardItemInterface } from '../types';
 import { debounce } from '../utils';
 import { trackingSource } from '../17appTrack';
 
+/**
+ * 篩選 (search filter) leaderboard 資料<br />
+ * 傳送追蹤篩選行為到 firebase
+ * @param initialData 未經過濾的 leaderboard 資料
+ * @returns data: 取得過濾後的 leaderboard 資料, handleOnChange: handle filter 資料的 method
+ */
 export const useFilter = (initialData: LeaderboardItemInterface[]) => {
   const [data, setData] = useState<LeaderboardItemInterface[]>(initialData);
   const [keyword, setKeyword] = useState('');
