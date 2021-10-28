3488f858e872895643ea935405e7857b8c3c803b
David Hung <davidhung@17.media>
2021-10-28 12:03:15 +0800

    feat: [APP-28305] finished useStartRender typeDoc

--- lib/hooks/useStartRender.ts
+++ lib/hooks/useStartRender.ts
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react';
 
+/**
+ * 設定 startRender 為 true 並且回傳
+ * @returns startRender
+ */
 const useStartRender = () => {
   const [startRender, setStartRender] = useState(false);
   useEffect(() => {
