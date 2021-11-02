20fdbb041dc32b135da04338d060fe0f50dc0b0e
David Hung <davidhung@17.media>
2021-11-02 14:58:19 +0800

    feat: [APP-28420] useTabLocked typedoc

--- lib/hooks/useTabLocked.ts
+++ lib/hooks/useTabLocked.ts
@@ -1,6 +1,11 @@
 import { useState, useEffect, useRef, useCallback } from 'react';
 import { now } from '@17media/dad';
 
+/**
+ * 用日期判斷是否要鎖tab
+ * @param lockDate string ex: 2020-04-10T23:59:59+08:00
+ * @returns locked, 是否鎖定
+ */
 const useTabLocked = (lockDate: string) => {
   const getLockedStatus = useCallback(() => {
     const lockTime = new Date(lockDate);
