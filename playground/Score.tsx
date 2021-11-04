565cfef0da96fdc6bd7c72d0ed737c8057dce167
sky790312 <sky790312yks@gmail.com>
2021-11-04 16:59:31 +0800

    fix: number maximum bug

--- playground/Score.tsx
+++ playground/Score.tsx
@@ -5,17 +5,22 @@ const Follower = () => {
   const [inputScore, setInputScore] = useState<number>(0);
   const score = useScore(inputScore);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newScore = e.target.value;
+    if (newScore.length > 10) {
+      return;
+    }
+
+    setInputScore(+newScore);
+  };
+
   return (
     <div>
       <h3>
         useScore will animate to change the number and remove digit number.
       </h3>
-      <p>Please enter a number</p>
-      <input
-        type="number"
-        value={inputScore}
-        onChange={e => setInputScore(+e.target.value)}
-      />
+      <p>Please enter a natural number (max 10 digit)</p>
+      <input type="number" value={inputScore} onChange={handleChange} />
       <p>score: {score}</p>
     </div>
   );
