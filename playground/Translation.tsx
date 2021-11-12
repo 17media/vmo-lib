a049e7f2f27039cc26c8f3ded68faf91f9d74849
David Hung <davidhung@17.media>
2021-11-12 11:37:10 +0800

    chore: [APP-29094] update playground

--- playground/Translation.tsx
+++ playground/Translation.tsx
@@ -9,26 +9,15 @@ const StyledInput = styled.input`
   width: 500px;
 `;
 
-const Checkbox = styled.input`
+const StyledCheckbox = styled.input`
   margin-right: 20px;
   padding: 5px 10px 5px 0;
 `;
 
 const Translation = () => {
-  const [eventoryType, setEventoryType] = useState<string>(
+  const [eventType, setEventoryType] = useState<string>(
     '12822-golden-feather-5',
   );
-  const [useTranslationProps, setUseTranslationProps] = useState<{
-    eventType: string;
-    supportLangs: RegionLanguage[];
-  }>({
-    eventType: '',
-    supportLangs: [],
-  });
-
-  const [supportLangs, setSupportLangs] = useState<RegionLanguage[]>([
-    RegionLanguage.JAPAN,
-  ]);
 
   const allSupportLanguages = [
     RegionLanguage.TAIWAN,
@@ -38,17 +27,28 @@ const Translation = () => {
     RegionLanguage.EUROPE,
     RegionLanguage.ARAB,
   ];
+  const [supportLangs, setSupportLangs] = useState<RegionLanguage[]>([
+    RegionLanguage.JAPAN,
+  ]);
 
-  const [selectedTranslationKey, setSelectedTranslationKey] =
-    useState<string>();
-  const [targetTranslation, setTargetTranslation] = useState<string>();
-
+  const [useTranslationProps, setUseTranslationProps] = useState<{
+    eventType: string;
+    supportLangs: RegionLanguage[];
+  }>({
+    eventType: '',
+    supportLangs: [],
+  });
   const translations = useTranslation(
     useTranslationProps.eventType,
     useTranslationProps.supportLangs,
   );
 
-  const eventoryTypeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
+  const [selectedTranslationKey, setSelectedTranslationKey] =
+    useState<string>();
+
+  const [targetTranslation, setTargetTranslation] = useState<string>();
+
+  const eventTypeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
     setEventoryType(e.target.value);
 
   const translationSelectHandler = e => {
@@ -72,7 +72,6 @@ const Translation = () => {
   };
 
   const submitHandler = () => {
-    const eventType = eventoryType;
     setUseTranslationProps({
       eventType,
       supportLangs,
@@ -83,19 +82,11 @@ const Translation = () => {
 
   return (
     <div>
-      <span>請輸入eventType：</span>
-      <StyledInput
-        type="text"
-        value={eventoryType}
-        placeholder="請輸入 Eventory Container ID"
-        onChange={eventoryTypeHandler}
-      />
-      <br />
-      <span>eventory 支援語系: </span>
+      <span>支援語系: </span>
       {allSupportLanguages.map(langCode => (
         <>
           <span>{langCode}</span>
-          <Checkbox
+          <StyledCheckbox
             type="checkbox"
             value={langCode}
             onChange={onChangeHandler}
@@ -104,9 +95,32 @@ const Translation = () => {
         </>
       ))}
       <br />
+      <span>eventType：</span>
+      <StyledInput
+        type="text"
+        value={eventType}
+        placeholder="請輸入 eventType"
+        onChange={eventTypeHandler}
+      />
+      <br />
       <button type="button" onClick={submitHandler}>
         送出
       </button>
+      <br />
+      <span>
+        注意：
+        <br />
+        1.
+        此功能必須在取得eventType的翻譯前先設定好支援語系，取得翻譯後，再去設定支援語系重新送出將不會重新翻譯，除非再次取得其他eventType的翻譯
+        <br />
+        2.
+        如果URL帶有翻譯參數，則會以URL參數為主，例如URL有帶&lang=zh_TW，即便是支援語系只設定支援ja，還是會以zh_TW做翻譯
+        <br />
+        3. 如果設定多個支援語系，則會以瀏覽器設定的最優先的偏好語言作翻譯
+        <br />
+        4.
+        如果設定的支援語系不再瀏覽器設定的偏好語言，則會以中文作為翻譯。例如僅設定支援日文(ja)，但回傳翻譯有中文，且瀏覽器裡沒有把日文設定成偏好語言，則會以中文作為翻譯
+      </span>
       <br />
       <br />
       <span>請選擇要翻譯的key: </span>
