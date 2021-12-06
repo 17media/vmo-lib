2f833b5d36be779cabfc7ecc728b7c4a54509237
sky790312 <sky790312yks@gmail.com>
2021-12-06 17:55:41 +0800

    Updates

--- output/bundle.js
+++ output/bundle.js
@@ -943,11 +943,9 @@ function isTrackingEvent(value) {
 function isMobile() {
   var check = false;
 
-  if (typeof window !== 'undefined' || typeof navigator !== 'undefined') {
-    (function (a) {
-      if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
-    })(navigator.userAgent || navigator.vendor || window.opera);
-  }
+  (function (a) {
+    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+  })(navigator.userAgent || navigator.vendor || window.opera);
 
   return check;
 }
@@ -7995,23 +7993,24 @@ var ScratchOffCard = function ScratchOffCard(_ref) {
       isCoverImageReady = _useState2[0],
       setIsCoverImageReady = _useState2[1];
 
-  (0,react__WEBPACK_IMPORTED_MODULE_2__.useEffect)(function () {
+  var handleCoverImgOnLoad = function handleCoverImgOnLoad() {
     var _canvasRef$current;
+
+    (_canvasRef$current = canvasRef.current) === null || _canvasRef$current === void 0 ? void 0 : _canvasRef$current.getContext('2d').drawImage(coverImgRef.current, 0, 0, width, height);
+    setIsCoverImageReady(true);
+  };
+
+  (0,react__WEBPACK_IMPORTED_MODULE_2__.useEffect)(function () {
+    var _canvasRef$current2;
 
     var isDrawing;
     var lastPoint;
     var canvas = canvasRef.current;
-    var ctx = (_canvasRef$current = canvasRef.current) === null || _canvasRef$current === void 0 ? void 0 : _canvasRef$current.getContext('2d');
+    var ctx = (_canvasRef$current2 = canvasRef.current) === null || _canvasRef$current2 === void 0 ? void 0 : _canvasRef$current2.getContext('2d');
     var image = coverImgRef.current;
     var brush = new Image();
-    ctx.drawImage(image, 0, 0, width, height);
-
-    image.onload = function () {
-      ctx.drawImage(image, 0, 0, width, height);
-      setIsCoverImageReady(true);
-    };
-
     brush.src = _brush_jpg__WEBPACK_IMPORTED_MODULE_3__.default;
+    ctx.drawImage(image, 0, 0, width, height);
 
     var handleMouseDown = function handleMouseDown(e) {
       isDrawing = true;
@@ -8019,7 +8018,7 @@ var ScratchOffCard = function ScratchOffCard(_ref) {
     };
 
     var handleMouseMove = function handleMouseMove(e) {
-      var _canvasRef$current2;
+      var _canvasRef$current3;
 
       if (!isDrawing) {
         return;
@@ -8042,11 +8041,11 @@ var ScratchOffCard = function ScratchOffCard(_ref) {
       lastPoint = currentPoint;
       var currentPercentage = (0,_utils__WEBPACK_IMPORTED_MODULE_4__.getFilledInPixels)(32, ctx, width, height);
 
-      if (currentPercentage > revealPercentage && (_canvasRef$current2 = canvasRef.current) !== null && _canvasRef$current2 !== void 0 && _canvasRef$current2.parentNode) {
-        var _canvasRef$current3;
+      if (currentPercentage > revealPercentage && (_canvasRef$current3 = canvasRef.current) !== null && _canvasRef$current3 !== void 0 && _canvasRef$current3.parentNode) {
+        var _canvasRef$current4;
 
         handleReveal();
-        (_canvasRef$current3 = canvasRef.current) === null || _canvasRef$current3 === void 0 ? void 0 : _canvasRef$current3.parentNode.removeChild(canvasRef.current);
+        (_canvasRef$current4 = canvasRef.current) === null || _canvasRef$current4 === void 0 ? void 0 : _canvasRef$current4.parentNode.removeChild(canvasRef.current);
       }
     };
 
@@ -8082,11 +8081,12 @@ var ScratchOffCard = function ScratchOffCard(_ref) {
     alt: "",
     ref: coverImgRef,
     src: coverImgSrc,
+    onLoad: handleCoverImgOnLoad,
     crossOrigin: "anonymous"
   }));
 };
 
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ScratchOffCard);
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (/*#__PURE__*/(0,react__WEBPACK_IMPORTED_MODULE_2__.memo)(ScratchOffCard));
 
 /***/ }),
 
@@ -9530,7 +9530,7 @@ var ScratchOff = function ScratchOff() {
   var _useState5 = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(defaultWidth),
       _useState6 = (0,_babel_runtime_helpers_slicedToArray__WEBPACK_IMPORTED_MODULE_0__.default)(_useState5, 2),
       inputWidth = _useState6[0],
-      setInputWdith = _useState6[1];
+      setInputWidth = _useState6[1];
 
   var _useState7 = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(defaultHeight),
       _useState8 = (0,_babel_runtime_helpers_slicedToArray__WEBPACK_IMPORTED_MODULE_0__.default)(_useState7, 2),
@@ -9550,7 +9550,7 @@ var ScratchOff = function ScratchOff() {
   var _useState13 = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(defaultWidth),
       _useState14 = (0,_babel_runtime_helpers_slicedToArray__WEBPACK_IMPORTED_MODULE_0__.default)(_useState13, 2),
       width = _useState14[0],
-      setWdith = _useState14[1];
+      setWidth = _useState14[1];
 
   var _useState15 = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(defaultHeight),
       _useState16 = (0,_babel_runtime_helpers_slicedToArray__WEBPACK_IMPORTED_MODULE_0__.default)(_useState15, 2),
@@ -9562,20 +9562,20 @@ var ScratchOff = function ScratchOff() {
       revealPercentage = _useState18[0],
       setRevealPercentage = _useState18[1];
 
-  var handleReveal = function handleReveal() {
+  var handleReveal = (0,react__WEBPACK_IMPORTED_MODULE_1__.useCallback)(function () {
     console.log('handle function after reveal.');
-  };
+  }, []);
 
   var handleResetDefault = function handleResetDefault() {
     setInputCoverImgSrc(defaultCoverImgSrc);
-    setInputWdith(defaultWidth);
+    setInputWidth(defaultWidth);
     setInputHeight(defaultHeight);
     setInputRevealPercentage(defaultRevealPercentage);
   };
 
   var handleGenerate = function handleGenerate() {
     setCoverImgSrc(inputCoverImgSrc);
-    setWdith(inputWidth);
+    setWidth(inputWidth);
     setHeight(inputHeight);
     setRevealPercentage(inputRevealPercentage);
     generateNewKey();
@@ -9611,7 +9611,7 @@ var ScratchOff = function ScratchOff() {
     type: "text",
     value: inputWidth,
     onChange: function onChange(e) {
-      return setInputWdith(+e.target.value);
+      return setInputWidth(+e.target.value);
     }
   }), ' ', "height:", ' ', /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("input", {
     type: "text",
@@ -9633,7 +9633,7 @@ var ScratchOff = function ScratchOff() {
   }, "generate"), ' '), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("p", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("button", {
     type: "button",
     onClick: handleResetDefault
-  }, "reset default setting")), revealPercentage ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement(_lib_components_ScratchOffCard__WEBPACK_IMPORTED_MODULE_2__.default, {
+  }, "reset default setting")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement(_lib_components_ScratchOffCard__WEBPACK_IMPORTED_MODULE_2__.default, {
     key: key,
     width: width,
     height: height,
@@ -9643,7 +9643,7 @@ var ScratchOff = function ScratchOff() {
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("h2", null, "Congratulations!"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("h1", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("code", null, "Coupon code : 1651613335")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("button", {
     type: "button",
     onClick: handleGenerate
-  }, "replay again"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("p", null, "same as generate behavior outside(using changing key to replay.)")))) : /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement(react__WEBPACK_IMPORTED_MODULE_1__.Fragment, null));
+  }, "replay again"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("p", null, "same as generate behavior outside(using changing key to replay.)")))));
 };
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.memo(ScratchOff));
