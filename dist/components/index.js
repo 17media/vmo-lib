f816101f78eff2c7b7cd207b2838e8ef7f1e4c4b
Jeff <b3589481400@gmail.com>
2021-12-15 13:43:28 +0800

    revert dist

--- dist/components/index.js
+++ dist/components/index.js
@@ -9,18 +9,10 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const TransitionLeaderboardWrapper_1 = require("./TransitionLeaderboardWrapper");
 const VirtualizedList_1 = require("./VirtualizedList");
-const ScratchOffCard_1 = __importDefault(require("./ScratchOffCard"));
 __exportStar(require("./TransitionLeaderboardWrapper"), exports);
 __exportStar(require("./VirtualizedList"), exports);
-exports.default = {
-    TransitionLeaderboardWrapper: TransitionLeaderboardWrapper_1.TransitionLeaderboardWrapper,
-    VirtualizedList: VirtualizedList_1.VirtualizedList,
-    ScratchOffCard: ScratchOffCard_1.default,
-};
+exports.default = { TransitionLeaderboardWrapper: TransitionLeaderboardWrapper_1.TransitionLeaderboardWrapper, VirtualizedList: VirtualizedList_1.VirtualizedList };
 //# sourceMappingURL=index.js.map
