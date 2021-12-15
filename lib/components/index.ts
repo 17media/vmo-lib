c90d791875d7e01cca6425f00f07efbb3a8c647d
Jeff <b3589481400@gmail.com>
2021-12-15 12:48:15 +0800

    [modi] correct type

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -1,7 +1,12 @@
 import { TransitionLeaderboardWrapper } from './TransitionLeaderboardWrapper';
 import { VirtualizedList } from './VirtualizedList';
+import ScratchOffCard from './ScratchOffCard';
 
 export * from './TransitionLeaderboardWrapper';
 export * from './VirtualizedList';
 
-export default { TransitionLeaderboardWrapper, VirtualizedList };
+export default {
+  TransitionLeaderboardWrapper,
+  VirtualizedList,
+  ScratchOffCard,
+};
