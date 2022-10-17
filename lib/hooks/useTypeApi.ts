4eeb86dbdd7154cf65dd00dcdbe62531f3557045
David Hung <davidhung@17.media>
2022-10-17 17:55:50 +0800

    spike: [APP-52104] plan c debug and organize code

--- lib/hooks/useTypeApi.ts
+++ lib/hooks/useTypeApi.ts
@@ -53,8 +53,8 @@ export const useTypeApi = (
   const [polling, setPolling] = useState(false);
   const [requestError, setRequestError] = useState<any | null>(null);
   const [leaderboardData, setLeaderboardData] = useState(initialData);
-  const [cacheData, setCacheData] = useState<any[]>([]);
-  const [networkData, setNetworkData] = useState<any[]>([]);
+  const [cacheData, setCacheData] = useState<User[][]>([]);
+  const [networkData, setNetworkData] = useState<User[][]>([]);
   const [suspend, setSuspend] = useState(false);
   const [options, setOptions] = useState<any[]>(
     apiList.map(() => ({
@@ -66,14 +66,6 @@ export const useTypeApi = (
 
   const [reload, setReload] = useState(false);
 
-  // const isFirstInit = useRef<boolean[]>(apiList.map(() => true));
-
-  // const isFirstInitErrorRef = useRef<boolean[]>(apiList.map(() => false));
-
-  // const shouldDelayRef = useRef<boolean[]>(apiList.map(() => false));
-
-  // const isFirstInit = useRef(true);
-
   const isFirstInitErrorRef = useRef(false);
 
   const shouldDelayRef = useRef(false);
@@ -83,147 +75,158 @@ export const useTypeApi = (
     [],
   );
 
-  const getDataRealTimeAPI = useCallback(
-    (apis: APIType[] = [], strategy: CacheStrategy) =>
-      async () => {
-        setRequestError(null);
-        if (isFirstInit.current) {
-          setLoading(true);
-        } else {
-          setPolling(true);
-        }
-        const apiPromiseList = apis
-          .map((type: APIType, index) => {
-            if (options[index].noMore) return null;
-            return getLeaderboardEventory({
-              type,
-              cancelToken: source.current[index]!.token,
-              limit: options[index].limit,
-              cursor: options[index].cursor,
-              withoutOnliveInfo: options[index].withoutOnliveInfo,
-              strategy,
-            });
-          })
-          .filter(Boolean);
+  const getLeaderboardData = useCallback(
+    async (apis: APIType[] = [], strategy: CacheStrategy) => {
+      setRequestError(null);
+      if (isFirstInit.current) {
+        setLoading(true);
+      } else {
+        setPolling(true);
+      }
+      const apiPromiseList = apis
+        .map((type: APIType, index) => {
+          if (options[index].noMore) return null;
+          return getLeaderboardEventory({
+            type,
+            cancelToken: source.current[index]!.token,
+            limit: options[index].limit,
+            cursor: options[index].cursor,
+            withoutOnliveInfo: options[index].withoutOnliveInfo,
+            strategy,
+          });
+        })
+        .filter(Boolean);
 
-        if (apiPromiseList.length > 0) {
-          try {
-            const results = await Promise.all(apiPromiseList);
+      if (apiPromiseList.length > 0) {
+        try {
+          const results = await Promise.all(apiPromiseList);
 
-            if (cacheStrategy === CacheStrategy.CACHE_THEN_NETWORK) {
-              /**
-               * 首筆資料一定是先使用 cache，之後的資料是看 callbackResponses 回應模式
-               * */
-              setCacheData(pre =>
-                results.map((result, index) => {
-                  if (pre[index]) {
-                    return [...pre[index], ...result.data.data.data];
-                  }
+          if (cacheStrategy === CacheStrategy.CACHE_THEN_NETWORK) {
+            /**
+             * 首筆資料一定是先使用 cache，之後的資料是看 callbackResponses 回應模式
+             * */
+            setCacheData(pre =>
+              results.map((result, index) => {
+                if (result.data) {
                   const nextCache = [...result.data.data.data];
+                  if (pre[index]) {
+                    return [...pre[index], ...nextCache];
+                  }
                   return nextCache;
+                }
+                return [];
+              }),
+            );
+
+            if (isFirstInit.current) {
+              setLoading(false);
+            } else {
+              setPolling(false);
+            }
+
+            // 如果需要 delay 下一次的 request，且不是一開始就斷網，執行 delay
+            if (!isFirstInitErrorRef.current && shouldDelayRef.current) {
+              await sleep(1000);
+              setReload(false);
+            }
+
+            const networkCallbacks = results.map(result => result.callback);
+            const callbackResponses = await Promise.all(networkCallbacks);
+
+            // 紀錄是否一開始就斷網, 只要其中一個出錯就當作全部有問題
+            const callbacksError = callbackResponses.some(
+              callbackRes => callbackRes.error,
+            );
+            if (callbacksError && isFirstInit.current) {
+              isFirstInitErrorRef.current = true;
+            }
+
+            // 紀錄是否需要 delay 下一次的 request
+            shouldDelayRef.current = callbacksError;
+            if (!isFirstInitErrorRef.current && shouldDelayRef.current) {
+              setReload(true);
+            }
+
+            /**
+             * 理想情況：一開始立即顯示 cache data(100筆), 然後 network api 每一百筆更新畫面一次, 整個取代
+             * 讀到一半斷網：同理想情況, api 沒回來就不處理 例如顯示到500筆就是500筆
+             * 如果有其中一個api error沒有回傳data，就完全不更新資料，等到下一次callback成功才更新
+             * */
+            const callbacksData = callbackResponses.filter(
+              callbackRes => callbackRes.data,
+            );
+            if (
+              callbacksData.length === apiList.length &&
+              !isFirstInitErrorRef.current
+            ) {
+              setNetworkData(pre =>
+                callbackResponses.map((callbackRes, index) => {
+                  if (callbackRes.data) {
+                    const nextData = [...callbackRes.data.data.data];
+                    if (pre[index]) {
+                      return [...pre[index], ...nextData];
+                    }
+                    return nextData;
+                  }
+                  return [];
                 }),
               );
-
-              if (isFirstInit.current) {
-                setLoading(false);
-              } else {
-                setPolling(false);
-              }
-
-              // 如果需要 delay 下一次的 request，且不是一開始就斷網，執行 delay
-              if (!isFirstInitErrorRef.current && shouldDelayRef.current) {
-                await sleep(1000);
-                setReload(false);
-              }
-
-              const networkCallbacks = results.map(result => result.callback);
-              const callbackResponses = await Promise.all(networkCallbacks);
-
-              // 紀錄是否一開始就斷網
-              const callbacksError = callbackResponses.some(
-                callbackRes => callbackRes.error,
-              );
-              if (callbacksError && isFirstInit.current) {
-                isFirstInitErrorRef.current = true;
-              }
-
-              // 紀錄是否需要 delay 下一次的 request
-              shouldDelayRef.current = callbacksError;
-              if (!isFirstInitErrorRef.current && shouldDelayRef.current) {
-                setReload(true);
-              }
-
-              /**
-               * 理想情況：一開始立即顯示 cache data(100筆), 然後 network api 每一百筆更新畫面一次, 整個取代
-               * 讀到一半斷網：同理想情況, api 沒回來就不處理 例如顯示到500筆就是500筆
-               * */
-              const callbacksData = callbackResponses.filter(
-                callbackRes => callbackRes.data,
-              );
-              if (
-                callbacksData.length === apiList.length &&
-                !isFirstInitErrorRef.current
-              ) {
-                setNetworkData(pre =>
-                  callbackResponses.map((callbackRes, index) => {
-                    if (pre[index]) {
-                      return [...pre[index], ...callbackRes.data.data.data];
-                    }
-                    const nextCache = [...callbackRes.data.data.data];
-                    return nextCache;
-                  }),
-                );
-              }
-
-              /**
-               * 一開始就斷網: 立即顯示 cache data(all) 全部，只有一開始就斷網會使用 cache 剩餘的資料
-               * 讀到一半斷網雖然也會回傳 callbackResponses?.error，但不是一開始就斷網，所以不會set cache data，會一直重新觸發load()
-               * */
-              if (isFirstInitErrorRef.current) {
-                setNetworkData(pre =>
-                  callbackResponses.map((callbackRes, index) => {
-                    if (pre[index]) {
-                      return [...pre[index], ...callbackRes.cache.data.data];
-                    }
-                    const nextCache = [...callbackRes.cache.data.data];
-                    return nextCache;
-                  }),
-                );
-              }
-
-              const nextOptions = callbackResponses.map(callbackRes => ({
-                limit: opt.limit,
-                cursor: isFirstInitErrorRef.current
-                  ? callbackRes.cache.data.nextCursor
-                  : callbackRes.data.data.nextCursor,
-                withoutOnliveInfo: opt.withoutOnliveInfo,
-                noMore: isFirstInitErrorRef.current
-                  ? !callbackRes.cache.data.nextCursor
-                  : !callbackRes.data.data.nextCursor,
-              }));
-
-              if (isFirstInitErrorRef.current || !shouldDelayRef.current) {
-                setOptions(nextOptions);
-              }
-            } else {
-              setLeaderboardData(results.map(result => result.data.data.data));
             }
-          } catch (error) {
-            setRequestError(error);
-          } finally {
-            setPolling(false);
-            setLoading(false);
-            isFirstInit.current = false;
+
+            /**
+             * 一開始就斷網: 立即顯示 cache data(all) 全部，只有一開始就斷網會使用 cache 剩餘的資料
+             * 讀到一半斷網雖然也會回傳 callbackResponses?.error，但不是一開始就斷網，所以不會set cache data，會一直重新觸發load()
+             * */
+            if (isFirstInitErrorRef.current) {
+              setNetworkData(pre =>
+                callbackResponses.map((callbackRes, index) => {
+                  if (callbackRes.cache) {
+                    const nextCache = [...callbackRes.cache.data.data];
+                    if (pre[index]) {
+                      return [...pre[index], ...nextCache];
+                    }
+                    return nextCache;
+                  }
+                  return [];
+                }),
+              );
+            }
+
+            const nextOptions = callbackResponses.map(callbackRes => {
+              const cursor = isFirstInitErrorRef.current
+                ? callbackRes.cache.data.nextCursor
+                : callbackRes.data.data.nextCursor;
+
+              return {
+                limit: opt.limit,
+                cursor,
+                withoutOnliveInfo: opt.withoutOnliveInfo,
+                noMore: !cursor,
+              };
+            });
+
+            if (isFirstInitErrorRef.current || !shouldDelayRef.current) {
+              setOptions(nextOptions);
+            }
+          } else {
+            setLeaderboardData(results.map(result => result.data.data.data));
           }
+        } catch (error) {
+          setRequestError(error);
+        } finally {
+          setPolling(false);
+          setLoading(false);
+          isFirstInit.current = false;
         }
-      },
-    [opt.cursor, opt.limit, opt.withoutOnliveInfo, options],
+      }
+    },
+    [apiList.length, cacheStrategy, opt.limit, opt.withoutOnliveInfo, options],
   );
 
-  const handleCacheStrategy = useCallback(() => {
-    const cacheStrategyFn = getDataRealTimeAPI(apiList, cacheStrategy);
-    cacheStrategyFn();
-  }, [apiList, cacheStrategy, getDataRealTimeAPI]);
+  const cancelAll = () => {
+    console.log('cancel all');
+    source.current.map(s => s.cancel());
+  };
 
   useEffect(() => {
     const handleVisibilityChange = () => {
@@ -250,22 +253,18 @@ export const useTypeApi = (
 
   useEffect(() => {
     if (!polling && !loading) {
-      handleCacheStrategy();
+      getLeaderboardData(apiList, cacheStrategy);
       return () => {
         if (polling || loading) cancelAll();
       };
     }
   }, [options]);
 
-  const cancelAll = () => {
-    source.current.map(s => s.cancel());
-  };
-
   useEffect(() => {
     if (reload) {
-      handleCacheStrategy();
+      getLeaderboardData(apiList, cacheStrategy);
     }
-  }, [reload]);
+  }, [apiList, cacheStrategy, getLeaderboardData, reload]);
 
   // 取得LB資料
   useEffect(() => {
@@ -274,18 +273,20 @@ export const useTypeApi = (
     if (polling) return;
     if (!polling && realTime > 0) {
       if (timeoutKey.current) clearTimeout(timeoutKey.current);
-      timeoutKey.current = window.setTimeout(handleCacheStrategy, realTime);
+      timeoutKey.current = window.setTimeout(
+        () => getLeaderboardData(apiList, cacheStrategy),
+        realTime,
+      );
     }
   }, [
     apiList,
     cacheStrategy,
-    getDataRealTimeAPI,
-    handleCacheStrategy,
+    getLeaderboardData,
     loading,
     polling,
     realTime,
     suspend,
-    JSON.stringify(options),
+    options,
   ]);
   return { loading, polling, requestError, leaderboardData };
 };
