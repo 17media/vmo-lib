284030442d5a363edf571d1300a3bf64e4044f19
sky790312 <sky790312yks@gmail.com>
2022-11-28 16:07:24 +0800

    feat: add sentry lib and playground

---
+++ playground/Sentry.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import useSentry from '../lib/hooks/useSentry';
+
+const Follower = () => {
+  useSentry();
+
+  const handleSentry = () => console.log(nonExistVariable);
+
+  return (
+    <div>
+      <h3>
+        useSentry can record error in{' '}
+        <a href="https://sentry.io/organizations/17live/issues/?referrer=sidebar">
+          sentry dashboard
+        </a>{' '}
+        filter by product tag `&quot;`eventory/custom`&quot;`.
+      </h3>
+      <p>Following button will trigger error and send it to sentry.</p>
+      <button onClick={handleSentry}>trigger error button</button>
+    </div>
+  );
+};
+
+export default React.memo(Follower);
