88f9906b4503e371cb3b43d607f0c6ddcda9350c
David Hung <davidhung@17.media>
2023-06-15 14:37:49 +0800

    tech: [APP-73754] update follower test

--- lib/hooks/__test__/follower.test.ts
+++ lib/hooks/__test__/follower.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, waitFor } from '@testing-library/react';
 import useFollower from '../useFollower';
 
 describe('test follower hook', () => {
@@ -6,37 +6,34 @@ describe('test follower hook', () => {
     const userID = '3a4464b5-8228-4d96-bd82-c0fe5e8be673';
     const accessToken = 'fe45a40b-962d-44f4-acac-a8f5362fe611';
 
-    const { result, waitForNextUpdate } = renderHook(() =>
-      useFollower(userID, accessToken),
-    );
+    const { result } = renderHook(() => useFollower(userID, accessToken, ''));
 
-    await waitForNextUpdate({ timeout: 5000 });
-    expect(result.current.errorMsg).toEqual('');
-    expect(result.current.followers.length).toBeGreaterThan(0);
+    await waitFor(() => expect(result.current.errorMsg).toEqual(''));
+    await waitFor(() =>
+      expect(result.current.followers.length).toBeGreaterThan(0),
+    );
   });
 
   test('should get empty followers array and errorMsg when user token invalid.', async () => {
     const userID = '3a4464b5-8228-4d96-bd82-c0fe5e8be673';
     const accessToken = 'wrong token';
 
-    const { result, waitForNextUpdate } = renderHook(() =>
-      useFollower(userID, accessToken),
-    );
+    const { result } = renderHook(() => useFollower(userID, accessToken, ''));
 
-    await waitForNextUpdate({ timeout: 5000 });
-    expect(result.current.errorMsg).toEqual('invalid token');
-    expect(result.current.followers).toEqual([]);
+    await waitFor(() =>
+      expect(result.current.errorMsg).toEqual('invalid token'),
+    );
+    await waitFor(() => expect(result.current.followers).toEqual([]));
   });
 
   test('should get empty followers array and errorMsg when no given user.', async () => {
     const userID = '';
     const accessToken = '';
 
-    const { result, waitForNextUpdate } = renderHook(() =>
-      useFollower(userID, accessToken),
+    const { result } = renderHook(() => useFollower(userID, accessToken, ''));
+    await waitFor(() =>
+      expect(result.current.errorMsg).toEqual('empty userID'),
     );
-    await waitForNextUpdate();
-    expect(result.current.errorMsg).toEqual('something wrong!');
-    expect(result.current.followers).toEqual([]);
+    await waitFor(() => expect(result.current.followers).toEqual([]));
   });
 });
