00a18e94725a80ac544e3cb38afdd51b5d49a0e9
David Hung <davidhung@17.media>
2023-07-03 10:56:45 +0800

    Updates

---
+++ out/_next/static/f8cIJNt327FsIEO5xkQxD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/627-cc744158f19a8a4b.js","static/chunks/pages/index-de589b6c49fceafe.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
