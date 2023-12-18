a7703db2341eed5655843bb6f23986b905f18ead
Jeff <b3589481400@gmail.com>
2023-12-18 19:11:22 +0800

    Updates

--- pets/cat.js
+++ pets/cat.js
@@ -1,12 +1,11 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.meow = void 0;
-const uuid_1 = __importDefault(require("uuid"));
+// import uuid from 'uuid';
+const uuid_1 = require("uuid");
 const meow = () => {
-    const id = uuid_1.default.v4();
+    // const id = uuid.v4();
+    const id = (0, uuid_1.v4)();
     console.log('meow !');
     return id;
 };
