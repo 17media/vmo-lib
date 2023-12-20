a252e78680b418c7311793eabb0227de08ced024
Jeff <b3589481400@gmail.com>
2023-12-20 20:13:41 +0800

    Updates

--- hooks/index.d.ts
+++ hooks/index.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="react" />
 export * from './useCountdown';
 export * from './useItemTransition';
 export * from './usePageData';
@@ -12,73 +11,3 @@ export * from './useScrollToLoadingContainer';
 export * from './useKeyboard';
 export * from './useFollower';
 export * from './useCheckWebview';
-declare const _default: {
-    useAutoNext: (isEnded: boolean, nextPage: number) => void;
-    useCountdown: (start: number, end: number, timeEndText: string) => {
-        status: import("./useCountdown").TimeStatus;
-        text: string;
-    };
-    useItemTransition: (itemStyle: import("./useItemTransition").ItemStyle, transition: import("./useItemTransition").TransitionStyle, rowItems: number, currentRank: number | number[]) => {
-        itemTransitionStyle: {
-            transition: string;
-            left: number;
-            top: number;
-        };
-    } | {
-        itemTransitionStyle: {
-            transition: string;
-            left: number;
-            top: number;
-            position: string;
-        }[];
-    };
-    useMockLeaderboard: (enable?: boolean, initMockList?: boolean, stable?: boolean, limit?: number) => {
-        readonly leaderboard: import("..").User[];
-    };
-    usePageData: ({ startDate, endDate, nextPage, isResultPage, endedText, test, init, }: import("./usePageData").PageContext) => {
-        mockLeaderboard: import("..").User[];
-        countdownText: string;
-        status: import("./useCountdown").TimeStatus;
-    };
-    useLuckyDraw: (allCandidates: import("..").User[], willAutoDrawRemainCount?: Boolean | undefined) => {
-        candidates: import("..").User[];
-        winners: import("..").User[];
-        allWinners: import("..").User[][];
-        draw: (roundWinnersCount: number) => void;
-        clearWinners: () => void;
-        reset: () => void;
-        currentRound: number;
-        hasDraw: Boolean;
-        MaskDiv: import("react").FC<{}>;
-    };
-    useSyncScroll: () => {
-        handleScroll: (e: any) => void;
-        elPoolRef: import("react").MutableRefObject<Element[]>;
-    };
-    useTypeApi: ({ apiList, realTime, initialData, cacheStrategy, opt, env, }: {
-        apiList: import("./useTypeApi").APIType[];
-        realTime: number;
-        initialData?: import("..").User[][] | undefined;
-        cacheStrategy?: import("./useTypeApi").CacheStrategy | undefined;
-        opt?: import("..").EventoryApiOption | undefined;
-        env?: import("..").Env | undefined;
-    }) => {
-        loading: boolean;
-        polling: boolean;
-        requestError: any;
-        leaderboardData: import("..").User[][] | undefined;
-    };
-    useFilter: (initialData: import("..").User[]) => {
-        data: import("..").User[];
-        handleOnChange: (value: any) => void;
-    };
-    useScrollToLoadingContainer: (loading: boolean) => void;
-    useKeyboard: (settings: import("./useKeyboard").ISettings) => void;
-    useFollower: (userID: string, accessToken: string, jwtAccessToken: string) => {
-        followers: string[];
-        errorMsg?: string | undefined;
-    };
-    useCheckWebview: () => boolean;
-    useCacheImage: (imageUrls: string[]) => Record<string, string>;
-};
-export default _default;
--- hooks/index.js
+++ hooks/index.js
@@ -1,17 +1,17 @@
-import { useAutoNext } from './useAutoNext';
-import { useCountdown } from './useCountdown';
-import { useItemTransition } from './useItemTransition';
-import { useMockLeaderboard } from './useMockLeaderboard';
-import { usePageData } from './usePageData';
-import { useLuckyDraw } from './useLuckyDraw';
-import { useSyncScroll } from './useSyncScroll';
-import { useTypeApi } from './useTypeApi';
-import { useFilter } from './useFilter';
-import { useScrollToLoadingContainer } from './useScrollToLoadingContainer';
-import { useKeyboard } from './useKeyboard';
-import { useFollower } from './useFollower';
-import { useCheckWebview } from './useCheckWebview';
-import { useCacheImage } from './useCacheImage';
+// import { useAutoNext } from './useAutoNext';
+// import { useCountdown } from './useCountdown';
+// import { useItemTransition } from './useItemTransition';
+// import { useMockLeaderboard } from './useMockLeaderboard';
+// import { usePageData } from './usePageData';
+// import { useLuckyDraw } from './useLuckyDraw';
+// import { useSyncScroll } from './useSyncScroll';
+// import { useTypeApi } from './useTypeApi';
+// import { useFilter } from './useFilter';
+// import { useScrollToLoadingContainer } from './useScrollToLoadingContainer';
+// import { useKeyboard } from './useKeyboard';
+// import { useFollower } from './useFollower';
+// import { useCheckWebview } from './useCheckWebview';
+// import { useCacheImage } from './useCacheImage';
 export * from './useCountdown';
 export * from './useItemTransition';
 export * from './usePageData';
@@ -25,20 +25,20 @@ export * from './useScrollToLoadingContainer';
 export * from './useKeyboard';
 export * from './useFollower';
 export * from './useCheckWebview';
-export default {
-    useAutoNext,
-    useCountdown,
-    useItemTransition,
-    useMockLeaderboard,
-    usePageData,
-    useLuckyDraw,
-    useSyncScroll,
-    useTypeApi,
-    useFilter,
-    useScrollToLoadingContainer,
-    useKeyboard,
-    useFollower,
-    useCheckWebview,
-    useCacheImage,
-};
+// export default {
+//   useAutoNext,
+//   useCountdown,
+//   useItemTransition,
+//   useMockLeaderboard,
+//   usePageData,
+//   useLuckyDraw,
+//   useSyncScroll,
+//   useTypeApi,
+//   useFilter,
+//   useScrollToLoadingContainer,
+//   useKeyboard,
+//   useFollower,
+//   useCheckWebview,
+//   useCacheImage,
+// };
 //# sourceMappingURL=index.js.map
