ab491f01c9af3d5b4eb772c1fd3298d7ce76dca0
KaiChang17 <kaichang@17.media>
2024-02-16 11:36:50 +0800

    fix: fix ESLint issue

--- lib/hooks/useLuckyDraw.tsx
+++ lib/hooks/useLuckyDraw.tsx
@@ -45,14 +45,12 @@ type Props = (
   MaskDiv: React.FC;
 };
 
-function MaskDiv() {
-  return (
-    <>
-      <StyledMaskDivOuter />
-      <StyledMaskDivInner />
-    </>
-  );
-}
+const MaskDiv = () => (
+  <>
+    <StyledMaskDivOuter />
+    <StyledMaskDivInner />
+  </>
+);
 
 /**
  * useLuckyDraw - pass all candidates, use the draw function with number of round winners to get each round winners, remain candidates and allWinners.
