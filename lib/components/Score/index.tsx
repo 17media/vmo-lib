e63b2c0189332c87c0bd7ba8e0d4c6342022d7a3
David Hung <davidhung@17.media>
2025-08-19 15:32:46 +0800

    [APP-114644] add score component

---
+++ lib/components/Score/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import useScore from '../../hooks/useScore';
+
+interface IScoreProps {
+  format: string;
+  value: string;
+  useAnimation?: boolean;
+  duration?: number;
+  className?: string;
+}
+
+function formatString(text: string, ...args: any[]) {
+  return text.replace(/{(\d+)}/g, (_, val: string) => args[parseInt(val, 10)]);
+}
+
+function toLocaleString(value: string): string {
+  return parseFloat(value).toLocaleString();
+}
+
+const Value = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+export const Score: React.FC<IScoreProps> = ({
+  format,
+  value,
+  useAnimation,
+  duration,
+  className,
+}) => {
+  const animatedValue = useScore({
+    givenScore: parseFloat(value),
+    duration,
+  });
+
+  const displayValue = useAnimation ? animatedValue : toLocaleString(value);
+  const formattedString = formatString(format, displayValue);
+
+  return <Value className={className}>{formattedString}</Value>;
+};
+
+export default Score;
