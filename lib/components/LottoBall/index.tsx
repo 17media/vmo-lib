571d2394e741efabf0cf5492b21593df8513e3b4
davidhung17LIVE <88997624+davidhung17LIVE@users.noreply.github.com>
2025-08-20 13:55:11 +0800

    [APP-114643] add lotto ball component (#89)
    * [APP-114643] add lotto ball component
    
    * [APP-114643] add lotto ball component
    
    * [APP-114643] add lotto ball component
    
    * [APP-114643] resolve git comment

---
+++ lib/components/LottoBall/index.tsx
@@ -0,0 +1,119 @@
+import React, { useMemo } from 'react';
+import styled from 'styled-components';
+
+export enum BallType {
+  /** Self-selected number */
+  SelfPick = 'Self Pick',
+  /** Computer-selected number */
+  ComputerPick = 'Computer Pick',
+  /** Can select a number, but the user has not yet selected */
+  NotYetPick = 'Not Yet Pick',
+  /** Winning number */
+  WinningBall = 'Winning Ball',
+  /** Not eligible */
+  IneligibleBall = 'Ineligible Ball',
+}
+
+/**
+ * LottoBall image resource settings
+ */
+export type LottoBallImageSrcConfig = Record<BallType, string>;
+
+export interface Ball {
+  value: number | string;
+  type: BallType;
+}
+
+interface LottoBallProps {
+  ballImageSrc: string;
+}
+
+/**
+ * A component that renders a list of lotto balls with pre-configured images.
+ * The props for this component are defined in `ILottoBallListProps`.
+ */
+export type LottoBallListComponent = React.FC<ILottoBallListProps>;
+
+export interface ILottoBallListProps {
+  /** Custom styles for the ball list container. */
+  ballListStyle?: React.CSSProperties;
+  /** Custom styles for each individual ball. */
+  ballStyle?: React.CSSProperties;
+  /** The list of ball objects to be rendered. */
+  ballList: Ball[];
+  /** The total number of ball slots to display, including empty ones. */
+  maximumPick: number;
+  className?: string;
+}
+
+interface IBaseLottoBallListProps extends ILottoBallListProps {
+  /** The configuration mapping ball types to image sources. */
+  lottoBallSrcConfig: LottoBallImageSrcConfig;
+}
+
+const LottoBallListContainer = styled.div`
+  display: flex;
+`;
+
+const LottoBall = styled.div<LottoBallProps>`
+  background-image: url(${p => p.ballImageSrc});
+  background-size: contain;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BaseLottoBallList: React.FC<IBaseLottoBallListProps> = ({
+  ballListStyle,
+  ballStyle,
+  ballList,
+  lottoBallSrcConfig,
+  maximumPick,
+  className,
+}) => (
+  <LottoBallListContainer style={ballListStyle} className={className}>
+    {Array.from({ length: maximumPick }).map((_, index) => {
+      const ball = ballList[index];
+      if (ball) {
+        const key = `ball-${index}`;
+        return (
+          <LottoBall
+            key={key}
+            style={ballStyle}
+            ballImageSrc={lottoBallSrcConfig[ball.type]}
+          >
+            {ball.value}
+          </LottoBall>
+        );
+      }
+      const key = `ineligible-${index}`;
+      return (
+        <LottoBall
+          key={key}
+          style={ballStyle}
+          ballImageSrc={lottoBallSrcConfig[BallType.IneligibleBall]}
+        />
+      );
+    })}
+  </LottoBallListContainer>
+);
+
+/**
+ * A hook that returns a memoized LottoBallList component with a pre-configured set of ball images.
+ * This avoids re-creating the component on every render.
+ * @param lottoBallSrcConfig The configuration for the ball image sources.
+ * @returns A memoized, pre-configured LottoBallList component of type `LottoBallListComponent`.
+ */
+export const useCreateLottoBallList = (
+  lottoBallSrcConfig: LottoBallImageSrcConfig,
+): LottoBallListComponent =>
+  useMemo(() => {
+    const ConfiguredLottoBallList: LottoBallListComponent = props => (
+      <BaseLottoBallList {...props} lottoBallSrcConfig={lottoBallSrcConfig} />
+    );
+
+    ConfiguredLottoBallList.displayName = 'LottoBallList';
+
+    return ConfiguredLottoBallList;
+  }, [lottoBallSrcConfig]);
